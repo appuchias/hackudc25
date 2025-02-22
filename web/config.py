7df4d85ec09b6492660fc96e47561f3cbf69ae2b
Appuchia <appuchia@appu.ltd>
2025-02-22 10:56:05 +0100

    Base de la configuración

--- web/config.py
+++ web/config.py
@@ -1,4 +1,19 @@
+import logging, os
+
 with open(".env", "r") as f:
-    for line in f:
-        key, value = line.strip().split("=")
-        globals()[key] = value
+    env = dict([line.strip().split("=") for line in f.readlines()])
+
+if os.getenv("PUBLIC") is None:
+    os.environ["PUBLIC"] = env["PUBLIC"]
+if os.getenv("SECRET") is None:
+    os.environ["SECRET"] = env["SECRET"]
+if os.getenv("LOGLEVEL") is None:
+    os.environ["LOGLEVEL"] = env.get("LOGLEVEL", "INFO")
+
+PUBLIC = os.getenv("PUBLIC")
+SECRET = os.getenv("SECRET")
+
+logging.basicConfig(
+    level=os.getenv("LOGLEVEL", "INFO").upper(),
+    format="%(asctime)s [%(levelname)s] (%(name)s) %(message)s",
+)
